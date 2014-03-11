4b3d67fc4f17079bd2a7b9e1dc4328b536d5bb39
flpa <flpa.dev@zoho.com>
2014-03-11 01:00:19 +0100

    initial commit of bug-buster.py with some notes

---
+++ bug-buster.py
@@ -0,0 +1,9 @@
+# ideas: 
+# - function to find array in array? cant deal with stuff between legs i think
+# - regex? hard to generate? could it even match?
+# 
+# added complexity:
+# - rotation
+# - different look(s)
+# - partially covered bugs
+# - bugs next to each other

c14a67e83b33a5424c5b4a78504c34128ad6a39a
flpa <flpa.dev@zoho.com>
2014-03-21 15:16:42 +0100

    finish first version of flatten function

--- bugbuster.py
+++ bugbuster.py
@@ -37,13 +37,14 @@ def array_matches_template(array, template, ignored_elements=[]):
     return false
 
 def flatten(iterable):
-    """ A simplified function for flattening lists that works in our context
-    (only one level of nested lists), because the proposed solutions
-    covering general cases seem to rely on 2.6+ functionality, e.g.:
+    """ A simplified function for flattening an iterable that works
+    in our context (only one level of nested lists), because the
+    proposed solutions covering general cases seem to rely on 2.6+
+    functionality, e.g.:
     - http://docs.python.org/2/library/itertools.html#itertools.chain,
     - https://stackoverflow.com/a/2158532"""
-#    return [item for sublist in iterables for item in sublist]
-    return []
+    return [item for sublist in iterable for item in sublist]
+
 
 
 

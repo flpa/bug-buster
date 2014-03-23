90e4beac70bf41456d75c8ae85a0795399f570d6
flpa <flpa.dev@zoho.com>
2014-03-23 14:17:27 +0100

    remove unnecessary methods (flatten, array matches template)

--- bugbuster.py
+++ bugbuster.py
@@ -49,20 +49,6 @@ def equalp(a,b):
     # TODO Doesn't Python have this somewhere?
     return a == b
     
-def array_matches_template(array, template, predicate=equalp):
-    array_flat = flatten(array)
-    template_flat = flatten(template)
-
-    return [i for i,j in zip(array_flat, template_flat) if predicate(i,j)]
-
-def flatten(iterable):
-    """ A simplified function for flattening an iterable that works
-    in our context (only one level of nested lists), because the
-    proposed solutions covering general cases seem to rely on 2.6+
-    functionality, e.g.:
-    - http://docs.python.org/2/library/itertools.html#itertools.chain,
-    - https://stackoverflow.com/a/2158532"""
-    return [item for sublist in iterable for item in sublist]
 
 
 
--- tests.py
+++ tests.py
@@ -41,15 +41,5 @@ class LandscapeTests(unittest.TestCase):
 
         self.assertRaises(AssertionError, landscape.add_row, "way longer row")
         
-class Tests(unittest.TestCase):
-
-    def test_flatten(self):
-        result = flatten([[1,2],[3,4]])
-        self.assertEquals(result, [1,2,3,4]);
-
-    def test_array_matches_templaet(self):
-        result = array_matches_template([[1],[2]], [[1], [3]])
-        self.assertEquals(result, [1])
-        
 if __name__ == '__main__':
     unittest.main()
